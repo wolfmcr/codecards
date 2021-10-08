36c187ea221307676facac9e82878751c1d630db
Wolfgang-stack <wolfgang.a.mcrae@gmail.com>
2021-10-08 14:49:50 -0700

    Added meta tags and remove redux devtools

--- client/src/store.js
+++ client/src/store.js
@@ -9,10 +9,7 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(...middleware))
 );
 
 export default store;
